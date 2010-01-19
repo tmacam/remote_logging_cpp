39acab842be6a82d687d4edf6c1d29e7bf293fae
Tiago Alves Macambira <tmacam@burocrata.org>
2010-01-19 15:27:34 -0200

    Support for appending to a file

--- udp_logger.py
+++ udp_logger.py
@@ -2,16 +2,33 @@
 # -*- coding: utf-8 -*-
 
 from socket import socket, AF_INET, SOCK_DGRAM
+from optparse import OptionParser
 
 def create_udp_server_socket(endpoint):
     skt = socket(AF_INET, SOCK_DGRAM)
     skt.bind(endpoint)
     return skt
 
+def MakeOptionParser():
+    parser = OptionParser()
+    parser.add_option('-a', '--append', dest='append', metavar="FILE",
+            help="Append log data to this file")
+    return parser
+
 if __name__ == '__main__':
+    parser = MakeOptionParser()
+    options, args = parser.parse_args()
+    append_file = None
+    if options.append:
+        append_file = open(options.append, 'a', 0)
+
     ENDPOINT = ("", 3000)       # empty string == INADDR_ANY
     skt = create_udp_server_socket(ENDPOINT)
+
     while True:
         data, (host, port) = skt.recvfrom(1500)
-        print "(%s:%i) %s" % (host, port, data)
+        log_line = "(%s:%i) %s\n" % (host, port, data)
+        print log_line,
+        if append_file:
+            append_file.write(log_line)
 
